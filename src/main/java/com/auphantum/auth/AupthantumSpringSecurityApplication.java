614042aae6eceb0967b50d761e9059310581c171
Art Astillero <art_astillero@epam.com>
2017-03-23 10:50:28 +0100

    initial commit

---
+++ src/main/java/com/auphantum/auth/AupthantumSpringSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.auphantum.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AupthantumSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AupthantumSpringSecurityApplication.class, args);
+	}
+}
